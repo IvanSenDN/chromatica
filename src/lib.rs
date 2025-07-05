0da060de112d04f77ae3a23a5d6ad69cdec212c1
Иван Сен <ivansendn@gmail.com>
2025-07-06 02:45:49 +0300

    Update some imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod core;
+pub use core::browser::Browser;
 pub use core::chromatica::Chromatica;
+pub use core::element::Element;
 pub use core::page::Page;
 pub use core::{PrintToPDF, UserAgentBrandVersion, UserAgentMetadata, UserAgentOverride};
